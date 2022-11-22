fb6f5e7f31a02257fe6c091d626a2721aef8c166
BernardAlpha <529341916@qq.com>
2022-11-22 12:37:57 +0800

    NutUI引入

--- wx-web-frontend/src/pages/index/index.vue
+++ wx-web-frontend/src/pages/index/index.vue
@@ -1,6 +1,14 @@
 <template>
   <view class="index">
     <text>{{ msg }}</text>
+    <text>{{ msg }}</text>
+    <nut-button type="success">成功按钮</nut-button>
+    <nut-animate type='flicker' :loop='true'>
+      <nut-button type='primary'>flicker-擦亮</nut-button>
+    </nut-animate>
+    <nut-animate type='jump' :loop='true'>
+      <nut-button type='primary'>jump-跳跃</nut-button>
+    </nut-animate>
   </view>
 </template>
 
@@ -9,7 +17,7 @@ import { ref } from 'vue'
 import './index.less'
 
 export default {
-  setup () {
+  setup() {
     const msg = ref('Hello world')
     return {
       msg
