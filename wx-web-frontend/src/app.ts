320b8b7ae09ecfc4c2f38ccfbfd2d1a36e980cfe
KenalSky <529341916@qq.com>
2022-12-04 04:42:36 +0800

    add custom font

--- wx-web-frontend/src/app.ts
+++ wx-web-frontend/src/app.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './styles/common.scss'
+import Taro from '@tarojs/taro'
+import website from './config/website'
+Taro.loadFontFace({
+  global: true,
+  family: 'MissU',
+  source: `url("${website.staticPrefix}/public/font/MissU.ttf")`,
+})
 const App = createApp({
   onShow (options) {},
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
