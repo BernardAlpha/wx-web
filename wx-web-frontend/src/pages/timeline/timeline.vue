b7acc1b35aa7bc17074ffe1d62088fc700ccbdc0
BernardAlpha <529341916@qq.com>
2023-05-15 19:41:10 +0800

    Create timeline page

---
+++ wx-web-frontend/src/pages/timeline/timeline.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="my-page my-page-timeline">
+    <view class="content">
+      <invisible-module></invisible-module>
+      <view @click="textClick" style="width:100%; text-align: center; padding-top: 60%; font-size: 36rpx;">
+        {{ text }}
+      </view>
+
+    </view>
+
+    <nav-bar pageName="时间线" titleId="timeline"></nav-bar>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import navBar from "@/pages/components/navBar.vue";
+import invisibleModule from "@/pages/components/invisibleModule.vue";
+
+const text = ref('宝宝想不想看呀...')
+const textClick = () => {
+  text.value = '还没做好来嘻嘻嘻😙😙😙'
+}
+</script>
+
+<style lang="scss" scoped>
+.my-page-timeline {
+  background: none;
+}
+</style>
