612ea88ff8d495b7fd2b8c83e5b9e90e69c421ac
BernardAlpha <529341916@qq.com>
2023-05-27 01:42:09 +0800

    Add countdown days. Add hearbeat animation. Add scroll-view in index page

--- wx-web-frontend/src/pages/index/index.vue
+++ wx-web-frontend/src/pages/index/index.vue
@@ -1,19 +1,26 @@
 <template>
   <view class="my-page">
     <nav-bar pageName="麦兰镇炸鸡店" :titleId="here.pageName" :canBack="false" topShadowHeight="360rpx"></nav-bar>
-    <view class="content">
+    <scroll-view class="content" :show-scrollbar="false" :scroll-y="true">
       <invisible-module></invisible-module>
       <view class="module-box module-timing">
         <view class="timing-text">我们在一起</view>
         <view class="timekeeper">
           <view class="t-day">
-            <text>已经</text><text class="number">{{ here.timekeeper.day }}</text><text>天啦</text><text v-if="here.timekeeper.day > 0"></text>
+            <text class="texts">已经</text><view class="number">{{ here.timekeeper.day }}</view><text class="texts">天啦</text><text v-if="here.timekeeper.day > 0"></text>
           </view>
           <view class="t-others">
             <text class="number">{{ here.timekeeper.hour }}</text><text>小时</text><text v-if="here.timekeeper.hour > 0"></text>
             <text class="number">{{ here.timekeeper.min }}</text><text>分钟</text><text v-if="here.timekeeper.min > 0"></text>
             <text class="number">{{ here.timekeeper.sec }}</text><text>秒</text><text v-if="here.timekeeper.sec > 0"></text>
           </view>
+        </view>
+      </view>
+      <view class="module-box module-days-matter">
+        <view class="module-title">
+          DaysMatter
+          <!-- <view class="notice-content"><text>距离暑假还有</text><view>{{ reverseCountdownDays }}</view><text>天</text></view> -->
+          <view class="notice-content">距离暑假还有<text class="number">{{ reverseCountdownDays }}</text>天哟</view>
         </view>
       </view>
       <view class="module-box module-notice">
@@ -36,7 +43,7 @@
           <view class="menu-desc">{{ item.name }}</view>
         </view>
       </view>
-    </view>
+    </scroll-view>
   </view>
 </template>
 
@@ -64,6 +71,9 @@ const here = reactive({
     {name: '时间线', icon: 'function-timetable3.png', url: '/pages/timeline/timeline'}
   ]
 });
+
+const reverseCountdownDays = Math.ceil( ((new Date('2023-07-03')).getTime() - (new Date()).getTime()) / (1000 * 60 * 60 * 24) - 1)
+
 let switchChecked = ref(false);
 let systemInfo = uni.getSystemInfoSync();  // 系统信息
 
@@ -120,10 +130,23 @@ const menuItemClick = (url) => {
 
 <style lang="scss" scoped>
 @import '@/styles/variables.scss';
+
+@keyframes heartbeat {
+  0% {
+    transform: scale(0.8);
+  }
+  50% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0.8);
+  }
+}
 .my-page {
   background: linear-gradient(0deg, $themePink, rgba(255, 255, 255, 0));
   .content {
     margin: 0rpx 30rpx 30rpx 30rpx;
+    height: 100%;
     .module-timing {
       height: auto;
       text-align: center;
@@ -136,10 +159,15 @@ const menuItemClick = (url) => {
         padding: 10rpx;
         .t-day {
           font-size: 46rpx;
+          .texts {
+            vertical-align: super;
+          }
           .number {
             font-size: 140rpx;
             line-height: 120rpx;
             color: #f7547d;
+            display: inline-block;
+            animation: heartbeat 1s infinite;
           }
         }
         .t-others {
@@ -149,6 +177,18 @@ const menuItemClick = (url) => {
             font-size: 38rpx;
           }
         }
+      }
+    }
+    .module-days-matter {
+      min-height: 0rpx;
+      .notice-content {
+        margin-top: 10rpx;
+        font-size: 50rpx;
+        color: #f7547d;
+        text-align: center;
+          .number {
+            color: yellowgreen;
+          }
       }
     }
     .module-notice {
