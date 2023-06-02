0560ed4effcb4e43361a1483a8cfb71f2f577752
BernardAlpha <529341916@qq.com>
2023-06-02 17:58:33 +0800

    Timeline page develop

--- wx-web-frontend/src/pages/timeline/timeline.vue
+++ wx-web-frontend/src/pages/timeline/timeline.vue
@@ -1,12 +1,23 @@
 <template>
   <view class="my-page my-page-timeline">
-    <view class="content">
+    <!-- <view class="content"> -->
+    <scroll-view class="content" :show-scrollbar="false" :scroll-y="true">
       <invisible-module></invisible-module>
-      <view @click="textClick" style="width:100%; text-align: center; padding-top: 60%; font-size: 36rpx;">
+      <!-- <view @click="textClick" style="width:100%; text-align: center; padding-top: 60%; font-size: 36rpx;">
         {{ text }}
-      </view>
+      </view> -->
 
-    </view>
+      <view class="timeline">
+        <view class="timeline-item" v-for="(item, index) in timelineList" :key="index">
+          <view class="timeline-left">{{ item.date }}</view>
+          <view class="timeline-center"></view>
+          <view class="timeline-right">
+            <view>{{ item.title }}</view>
+            <view>{{ item.desc }}</view>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
 
     <nav-bar pageName="时间线" titleId="timeline"></nav-bar>
   </view>
@@ -16,15 +27,26 @@
 import { ref } from "vue";
 import navBar from "@/pages/components/navBar.vue";
 import invisibleModule from "@/pages/components/invisibleModule.vue";
+import timelineList from "@/data/timeline.json";
 
-const text = ref('宝宝想不想看呀...')
+const text = ref("宝宝想不想看呀...");
 const textClick = () => {
-  text.value = '还没做好来嘻嘻嘻😙😙😙'
-}
+  text.value = "还没做好来嘻嘻嘻😙😙😙";
+};
 </script>
 
 <style lang="scss" scoped>
 .my-page-timeline {
   background: none;
+  .content {
+    background-color: aqua;
+    // height: 100%;
+    .timeline {
+      padding: 40rpx;
+      background-color: bisque;
+      height: 100%;
+      display: flex;
+    }
+  }
 }
 </style>
