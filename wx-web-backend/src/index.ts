6f532cad54b8a42dd8ae0d4493e1e381f37c7d7f
BernardAlpha <529341916@qq.com>
2023-10-10 15:52:04 +0800

    Test /events/latestEvent api

--- wx-web-backend/src/index.ts
+++ wx-web-backend/src/index.ts
@@ -1,14 +1,34 @@
 import express from 'express';
+import mysql from 'mysql2'
 
-const App = express();
+const Pollos = express();
 
-App.get('/', (req, res) => {
+const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+const [MYSQL_HOST, MYSQL_PORT] = MYSQL_ADDRESS.split(":");
+console.log('MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_PORT', MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_PORT);
+const sqlPool = mysql.createPool({
+  host: MYSQL_HOST,  //'localhost',
+  port: Number(MYSQL_PORT),
+  user: MYSQL_USERNAME,  //'root',
+  password: MYSQL_PASSWORD,  //'password',
+  database: 'mailan_pollos'
+});
+
+Pollos.get('/', (req, res) => {
   res.send('Hello  World!');
 });
 
+Pollos.get('/events/latestEvent', (req, res) => {
+  console.log('/events/latestEvent--req',req.query);
+  sqlPool.query('select * from events', (err, results, fields) => {
+    console.log('results',results);
+    if (err) throw err;
+    res.json(results);
+  })
+});
 
 const port = process.env.PORT || 1003;
 
-App.listen(port, () => {
+Pollos.listen(port, () => {
   console.log('启动! 端口：', port);
 })
