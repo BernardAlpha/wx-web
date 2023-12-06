808bac7619da170759026b61f9f21d698ee5e452
BernardAlpha <529341916@qq.com>
2023-12-06 17:23:00 +0800

    feat: wx login13

--- wx-web-backend/src/index.ts
+++ wx-web-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import mysql from 'mysql2'
 import axios from 'axios';
+import https from 'https';
 
 const Pollos = express();
 Pollos.use(express.urlencoded({ extended: true }));
@@ -17,11 +18,6 @@ const sqlPool = mysql.createPool({
   database: 'mailan_pollos'
 });
 
-const axiosIns = axios.create({
-  httpsAgent: {
-    rejectUnauthorized: false,
-  },
-});
 
 Pollos.get('/', (req, res) => {
   res.send('Hello  World!');
@@ -43,25 +39,28 @@ Pollos.post('/auth/wxLogin', (req, res) => {
   console.log('/auth/wxLogin--res', res);
   const code = req.body.code || ''
   // 获取微信小程序用户的 openid 和 session_key
-  axiosIns.get('https://api.weixin.qq.com/sns/jscode2session', {
+  axios.get('https://api.weixin.qq.com/sns/jscode2session', {
     params: {
       appid: 'wx2d0c03028e32da41',
       secret: '5d5dc979fa601da1e4dff025a1af0540',
       js_code: code,
       grant_type: 'authorization_code',
-    }
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false
+    })
   }).then(wxRes => {
     console.log('wxRes', wxRes);
     const openid = wxRes.data.openid;
     const sessionKey = wxRes.data.session_key;
     // 先查询是否存在该用户
-    sqlPool.query(`SELECT * FROM user WHERE wx_openid = ${openid}`, (err, results:Array<object>, fields) => {
+    sqlPool.query(`SELECT * FROM user WHERE wx_openid = ${openid}`, (err, results: Array<object>, fields) => {
       console.log('user-select', results);
       if (err) throw err;
-      if(results.length > 0) {    // 存在则返回用户信息及token
+      if (results.length > 0) {    // 存在则返回用户信息及token
         res.json(results[0])
-      }else {                     // 不存在先注册
-        sqlPool.query(`INSERT INTO user (wx_openid) VALUES (${openid});`, (err, results:Array<object>, fields) => {
+      } else {                     // 不存在先注册
+        sqlPool.query(`INSERT INTO user (wx_openid) VALUES (${openid});`, (err, results: Array<object>, fields) => {
           console.log('user-insert', results);
           if (err) throw err;
         })
